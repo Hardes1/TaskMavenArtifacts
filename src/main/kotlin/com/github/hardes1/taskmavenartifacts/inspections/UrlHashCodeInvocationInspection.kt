965c546bd761680f5f2b60a80fa97bc805637b12
Ustinov George <ustinovgo@gmail.com>
2023-03-18 20:46:27 +0200

    Basic Implementation

---
+++ src/main/kotlin/com/github/hardes1/taskmavenartifacts/inspections/UrlHashCodeInvocationInspection.kt
@@ -0,0 +1,37 @@
+package com.github.hardes1.taskmavenartifacts.inspections
+
+import com.github.hardes1.taskmavenartifacts.InspectionBundle
+import com.intellij.codeInspection.AbstractBaseUastLocalInspectionTool
+import com.intellij.codeInspection.ProblemsHolder
+import com.intellij.psi.PsiElementVisitor
+import com.intellij.uast.UastHintedVisitorAdapter
+import org.jetbrains.uast.UCallExpression
+import org.jetbrains.uast.visitor.AbstractUastNonRecursiveVisitor
+
+private const val HASH_CODE_NAME = "hashCode"
+private const val URL_CANONICAL_NAME = "java.net.URL"
+
+class UrlHashCodeInvocationInspection : AbstractBaseUastLocalInspectionTool() {
+
+    override fun buildVisitor(holder: ProblemsHolder, isOnTheFly: Boolean): PsiElementVisitor {
+        return UastHintedVisitorAdapter.create(
+            holder.file.language,
+            object : AbstractUastNonRecursiveVisitor() {
+                override fun visitCallExpression(node: UCallExpression): Boolean {
+                    val name = node.methodName ?: return false
+                    val classRef = node.receiverType ?: return false
+                    if (name == HASH_CODE_NAME && classRef.canonicalText == URL_CANONICAL_NAME) {
+                        node.sourcePsi?.let {
+                            holder.registerProblem(
+                                it,
+                                InspectionBundle.message("inspection.URL.call.hashcode.explicit")
+                            )
+                        }
+                    }
+                    return true
+                }
+            }, arrayOf(UCallExpression::class.java)
+        )
+    }
+
+}
